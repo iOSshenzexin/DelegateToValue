b529743b85a125deace3612408346ce8201c4cbb
iOSshenzexin <1045250128@qq.com>
2016-05-18 18:34:12 +0800

    Initial Commit

---
+++ 代理传值/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  代理传值
+//
+//  Created by 杨晓婧 on 16/5/18.
+//  Copyright © 2016年 QingDaoShangTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 代理传值/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  代理传值
+//
+//  Created by 杨晓婧 on 16/5/18.
+//  Copyright © 2016年 QingDaoShangTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
